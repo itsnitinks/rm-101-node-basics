776930a2b5551ee9c053c960d200b1051614f5fe
Nitin K S <96114966+itsnitinks@users.noreply.github.com>
2022-06-20 20:39:06 +0530

    bug fixed-uploaded

--- src/server.js
+++ src/server.js
@@ -1,8 +1,51 @@
 // install and import express
-const express = () => {};
+let fs=require("fs")
+let filename="./assets/user.json"
+let file=require(filename)
+
+const express = require("express")
 let app = express();
 
 // Code here
+app.use(express.json())
 
 // Note: Do not remove this export statement
+
+app.get("/",async(req,res)=>{
+    try{
+            fs.readFile(__dirname+ "/assests/users.html","utf8",function(error,data){
+                if(error){
+                    return res.send(error)
+                }
+                return res.send(data)
+            })
+    }
+    catch(err){
+        return res.send(err)
+    }
+})
+
+app.get("/users",async(req,res)=>{
+    try{
+            fs.readFile(__dirname+ "/assests/users.json",function(error,data){
+                if(error){
+                    return res.send(error)
+                }
+                return res.send(data)
+            })
+    }
+    catch(err){
+        return res.send(err)
+    }
+})
+
+
+
+app.listen(8000,async()=>{
+    try{
+        console.log("listening on port 8000")
+    }catch(err){
+        console.log("error is",err)
+    }
+})
 module.exports = app;
